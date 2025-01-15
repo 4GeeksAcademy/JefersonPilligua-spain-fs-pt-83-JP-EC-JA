c26e4ae3f58cec9f782a2aa410f65751867000ea
Jeferson Pilligua <jefer.pilligua1509@gmail.com>
2025-01-15 15:13:14 +0000

    realizando la primera vista

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,17 +3,34 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+		<nav className="navbar bg-body-tertiary">
+			<div className="container-fluid px-5">
+				<div className="col-3">
+					<h1 className="h3">TIENDA</h1>
+				</div>
+				<div className="col-5">
+					<form className="d-flex" role="search">
+						<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+						<button className="btn btn-outline-success" type="submit">Search</button>
+					</form>
+				</div>
+				<div className="col-4 text-end">
+					<div class="btn-group px-3">
+						<button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+							CATEGORIA
+						</button>
+						<ul class="dropdown-menu">
+							<li><a class="dropdown-item" href="#">Action</a></li>
+							<li><a class="dropdown-item" href="#">Another action</a></li>
+							<li><a class="dropdown-item" href="#">Something else here</a></li>
+						</ul>
+					</div>
+					<i class="bi bi-person"></i>
+					<i class="bi bi-heart"></i>
+					<i class="bi bi-cart3"></i>
 				</div>
 			</div>
 		</nav>
 	);
 };
+
